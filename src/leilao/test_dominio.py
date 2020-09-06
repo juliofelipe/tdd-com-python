0fd0f2ae760ce8023359a0501fd63b7c973de237
Julio Felipe <julio.felipe@casaldev.com.br>
2020-09-06 08:01:57 -0300

    test: lances com tres usuarios

--- src/leilao/test_dominio.py
+++ src/leilao/test_dominio.py
@@ -4,7 +4,10 @@ from src.leilao.dominio import Lance, Usuario, Leilao, Avaliador
 
 
 class TestAvaliador(TestCase):
-    def test_avalia(self):
+
+     # test_quando_adicionados_em_ordem_crescente_deve_restornar_o_maior_e_o_menor_valor_de_um_lance
+
+    def test_deve_restornar_o_maior_e_o_menor_valor_de_um_lance_quando_adicionados_em_ordem_crescente(self):
         gui = Usuario('Gui')
         yuri = Usuario('Yuri')
 
@@ -26,7 +29,7 @@ class TestAvaliador(TestCase):
         self.assertEqual(menor_valor_esperado, avaliador.menor_lance)
         self.assertEqual(maior_valor_esperado, avaliador.maior_lance)
 
-    def test_avalia2(self):
+    def test_deve_restornar_o_maior_e_o_menor_valor_de_um_lance_quando_adicionados_em_ordem_decrescente(self):
         gui = Usuario('Gui')
         yuri = Usuario('Yuri')
 
@@ -43,6 +46,45 @@ class TestAvaliador(TestCase):
 
         menor_valor_esperado = 100.0
         maior_valor_esperado = 150.0
+
+        self.assertEqual(menor_valor_esperado, avaliador.menor_lance)
+        self.assertEqual(maior_valor_esperado, avaliador.maior_lance)
+
+    def test_dev_retornar_o_mesmo_valor_para_o_maior_e_menor_lance_quando_leilao_tiver_um_lance(self):
+        gui = Usuario('gui')
+
+        lance = Lance(gui, 150.0)
+
+        leilao = Leilao('Celular')
+        leilao.lances.append(lance)
+
+        avaliador = Avaliador()
+        avaliador.avalia(leilao)
+
+        self.assertEqual(150.0, avaliador.menor_lance)
+        self.assertEqual(150.0, avaliador.maior_lance)
+
+    def test_dev_retornar_o_maior_e_o_menor_valor_quando_o_leilao_tiver_tres_lances(self):
+
+        gui = Usuario('Gui')
+        yuri = Usuario('Yuri')
+        vini = Usuario('Vini')
+
+        lance_do_yuri = Lance(yuri, 100.0)
+        lance_do_gui = Lance(gui, 150.0)
+        lance_do_vini = Lance(vini, 200.0)
+
+        leilao = Leilao('Celular')
+
+        leilao.lances.append(lance_do_gui)
+        leilao.lances.append(lance_do_yuri)
+        leilao.lances.append(lance_do_vini)
+
+        avaliador = Avaliador()
+        avaliador.avalia(leilao)
+
+        menor_valor_esperado = 100.0
+        maior_valor_esperado = 200.0
 
         self.assertEqual(menor_valor_esperado, avaliador.menor_lance)
         self.assertEqual(maior_valor_esperado, avaliador.maior_lance)
